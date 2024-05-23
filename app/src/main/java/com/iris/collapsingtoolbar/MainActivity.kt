9f4eb83f08df11f6d18ad2c81a08e1a32aec3ca6
Iris Yen <Iris.yen@shoalter.com>
2024-05-23 15:09:00 +0800

    feat: status bar 文字顏色的改變

--- app/src/main/java/com/iris/collapsingtoolbar/MainActivity.kt
+++ app/src/main/java/com/iris/collapsingtoolbar/MainActivity.kt
@@ -1,43 +1,68 @@
 package com.iris.collapsingtoolbar
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsetsController
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.collapsingtoolbar.databinding.ActivityMainBinding
+import com.example.collapsingtoolbar.databinding.ViewCollapsingAppbarBinding
 import com.example.collapsingtoolbar.databinding.ViewSearchBarBinding
-import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var searchBarBinding: ViewSearchBarBinding
     private lateinit var listAdapter: HomeGameListAdapter
+    private lateinit var viewAppBarLayout: ViewCollapsingAppbarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setSupportActionBar(_binding.toolbar)
+        viewAppBarLayout = binding.appbarLayout
 
-        val appBarLayout = binding.appbarLayout
-        appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { it, verticalOffset ->
-            // 计算折叠比例
-            val collapsedPercentage: Float = (Math.abs(verticalOffset) / appBarLayout.totalScrollRange.toFloat())
 
-            // 检查折叠状态
-            if (collapsedPercentage == 0f) {
-                // 完全展开
-                Toast.makeText(this, "完全展開", Toast.LENGTH_LONG).show()
-            } else if (collapsedPercentage == 1f) {
-                // 完全折叠
-                Toast.makeText(this, "完全折叠", Toast.LENGTH_LONG).show()
+        val appBarLayout = viewAppBarLayout.appbarContainer.apply {
+            addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+                // 計算折疊比例
+                val collapsedPercentage: Float =
+                    abs(verticalOffset) / appBarLayout.totalScrollRange.toFloat()
+
+                // 獲取 WindowInsetsController
+                val windowInsetsController = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    window.insetsController
+                } else {
+                    null
+                }
+
+                // 檢查折疊狀態
+                when (collapsedPercentage) {
+                    0f -> {
+                        // 完全展開 > 淺色文字
+                        windowInsetsController?.setSystemBarsAppearance(
+                            0,
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                        )
+                    }
+
+                    1f -> {
+                        // 完全折叠 > 深色文字
+                        windowInsetsController?.setSystemBarsAppearance(
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                        )
+                    }
+                }
             }
-        })
+        }
 
-        searchBarBinding = binding.searchLayout
-        searchBarBinding.viewLayoutSearch.setOnClickListener {
-            Toast.makeText(this, "Search bar is clicked", Toast.LENGTH_LONG).show()
+        searchBarBinding = binding.searchLayout.apply {
+            viewLayoutSearch.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Search bar is clicked", Toast.LENGTH_LONG).show()
+            }
         }
 
         listAdapter = HomeGameListAdapter(getTestData).apply {
